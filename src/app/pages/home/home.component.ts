35fab1efcf18eb3ca12ebbce7c2908f9a23eeaaa
Nithin V <nithinviswanathan@wimoku.com>
2024-09-09 18:49:44 +0530

    feat: home changes

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -5,4 +5,55 @@ import { Component } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
-export class HomeComponent {}
+export class HomeComponent {
+  public skills = [
+    {
+      label: 'HTML',
+      icon: 'fa-brands fa-html5',
+      class:
+        'flex align-items-center justify-content-center w-3rem h-3rem border-circle absolute bg-black-alpha-30 icon text-white text-4xl',
+    },
+    {
+      label: 'CSS',
+      icon: 'fa-brands fa-css3-alt',
+      class:
+        'flex align-items-center justify-content-center w-3rem h-3rem border-circle absolute bg-black-alpha-30 icon text-white text-4xl',
+    },
+    {
+      label: 'JavaScript',
+      icon: 'fa-brands fa-js',
+      class:
+        'flex align-items-center justify-content-center w-3rem h-3rem border-circle absolute bg-black-alpha-30 icon text-white text-4xl',
+    },
+    {
+      label: 'Angular',
+      icon: 'fa-brands fa-angular',
+      class:
+        'flex align-items-center justify-content-center w-3rem h-3rem border-circle absolute bg-black-alpha-30 icon text-white text-4xl',
+    },
+    {
+      label: 'Vue',
+      icon: 'fa-brands fa-vuejs',
+      class:
+        'flex align-items-center justify-content-center w-3rem h-3rem border-circle absolute bg-black-alpha-30 icon text-white text-4xl',
+    },
+    {
+      label: 'Node',
+      icon: 'fa-brands fa-node-js',
+      class:
+        'flex align-items-center justify-content-center w-3rem h-3rem border-circle absolute bg-black-alpha-30 icon text-white text-4xl',
+    },
+    {
+      label: 'React',
+      icon: 'fa-brands fa-react',
+      class:
+        'flex align-items-center justify-content-center w-3rem h-3rem border-circle absolute bg-black-alpha-30 icon text-white text-4xl',
+    },
+    {
+      label: 'Flutter',
+      icon: 'fa-brands fa-flutter',
+      class:
+        'flex align-items-center justify-content-center w-3rem h-3rem border-circle absolute bg-black-alpha-30 icon text-white text-4xl',
+    },
+  ];
+}
