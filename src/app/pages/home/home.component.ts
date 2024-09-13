42487ef4e16025cafc6b870abee678ba044e165d
Nithin V <nithinviswanathan@wimoku.com>
2024-09-13 21:09:05 +0530

    home updates

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -6,54 +6,48 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.scss',
 })
 export class HomeComponent {
+  public iconClass =
+    'flex align-items-center justify-content-center w-3rem h-3rem -mt-4 -ml-4 border-circle absolute bg-black-alpha-30 icon text-white text-4xl';
   public skills = [
     {
       label: 'HTML',
       icon: 'fa-brands fa-html5',
-      class:
-        'flex align-items-center justify-content-center w-3rem h-3rem -mt-4 -ml-4 border-circle absolute bg-black-alpha-30 icon text-white text-4xl',
+      color: '#e94108',
     },
     {
       label: 'CSS',
       icon: 'fa-brands fa-css3-alt',
-      class:
-        'flex align-items-center justify-content-center w-3rem h-3rem -mt-4 -ml-4 border-circle absolute bg-black-alpha-30 icon text-white text-4xl',
+      color: '#5600f5',
     },
     {
       label: 'JavaScript',
       icon: 'fa-brands fa-js',
-      class:
-        'flex align-items-center justify-content-center w-3rem h-3rem -mt-4 -ml-4 border-circle absolute bg-black-alpha-30 icon text-white text-4xl',
+      color: '#f0f108',
     },
     {
       label: 'Angular',
       icon: 'fa-brands fa-angular',
-      class:
-        'flex align-items-center justify-content-center w-3rem h-3rem -mt-4 -ml-4 border-circle absolute bg-black-alpha-30 icon text-white text-4xl',
+      color: '#dd0031',
     },
     {
-      label: 'Vue',
+      label: 'Vue js',
       icon: 'fa-brands fa-vuejs',
-      class:
-        'flex align-items-center justify-content-center w-3rem h-3rem -mt-4 -ml-4 border-circle absolute bg-black-alpha-30 icon text-white text-4xl',
+      color: '#08c889',
     },
     {
-      label: 'Node',
+      label: 'Node js',
       icon: 'fa-brands fa-node-js',
-      class:
-        'flex align-items-center justify-content-center w-3rem h-3rem -mt-4 -ml-4 border-circle absolute bg-black-alpha-30 icon text-white text-4xl',
+      color: '#6fd208',
     },
     {
-      label: 'React',
+      label: 'React js',
       icon: 'fa-brands fa-react',
-      class:
-        'flex align-items-center justify-content-center w-3rem h-3rem -mt-4 -ml-4 border-circle absolute bg-black-alpha-30 icon text-white text-4xl',
+      color: '#7fddff',
     },
     {
       label: 'Flutter',
       icon: 'fa-brands fa-flutter',
-      class:
-        'flex align-items-center justify-content-center w-3rem h-3rem -mt-4 -ml-4 border-circle absolute bg-black-alpha-30 icon text-white text-4xl',
+      color: '#44b1fa',
     },
   ];
 }
