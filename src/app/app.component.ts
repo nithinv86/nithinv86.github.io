f6c1336db9de0de0c2485325b5d6264524edf8b3
Nithin V <nithinviswanathan@wimoku.com>
2025-01-05 16:33:29 +0530

    version upgrade to 19

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
-import { PrimeNGConfig } from 'primeng/api';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent implements OnInit {
-  constructor(private primengConfig: PrimeNGConfig) {}
-
-  ngOnInit() {
-    this.primengConfig.ripple = true;
-  }
+export class AppComponent {
+  title = 'nithin';
 }
--- src/app/pages/summary/summary.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-summary',
-  templateUrl: './summary.component.html',
-  styleUrl: './summary.component.scss',
-})
-export class SummaryComponent {}
