9192ac9087170807a546a68c0bec9abdfb4ac537
Nithin V <nithinviswanathan@wimoku.com>
2025-01-06 09:21:20 +0530

    details started

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+import { MenuItem } from 'primeng/api';
+import { Menu, MenuModule } from 'primeng/menu';
+import { TooltipModule } from 'primeng/tooltip';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [ButtonModule, MenuModule, TooltipModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
   standalone: true,
 })
-export class HeaderComponent {}
+export class HeaderComponent implements AfterViewInit {
+  public items: MenuItem[] | undefined;
+  @ViewChild('menu') menu!: Menu;
+
+  constructor() {
+    this.items = [
+      {
+        label: 'About',
+        icon: '&nbsp;',
+        routerLink: '/about',
+        shortcut: '⌘+A',
+      },
+      {
+        label: 'Projects',
+        icon: '&nbsp;',
+        routerLink: '/projects',
+        shortcut: '⌘+P',
+      },
+      {
+        label: 'Contact',
+        icon: '&nbsp;',
+        routerLink: '/contact',
+        shortcut: '⌘+C',
+      },
+    ];
+  }
+
+  ngAfterViewInit(): void {
+    const firstMenuItem = this.menu.el.nativeElement.querySelector('.p-menu-item');
+
+    if (firstMenuItem) {
+      firstMenuItem.classList.add('p-focus');
+    }
+  }
+}
