2a44d3a9f8dcb1a229d7cdc57e45f03453037091
Nithin V <nithinviswanathan@wimoku.com>
2025-01-16 19:41:29 +0530

    updates

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  {
+    path: 'home',
+    loadComponent: () => import('./components/home/home.component').then((m) => m.HomeComponent),
+  },
+  {
+    path: 'projects',
+    loadComponent: () =>
+      import('./components/projects/projects.component').then((m) => m.ProjectsComponent),
+  },
+  {
+    path: 'about',
+    loadComponent: () => import('./components/about/about.component').then((m) => m.AboutComponent),
+  },
+  {
+    path: 'contact',
+    loadComponent: () =>
+      import('./components/contact/contact.component').then((m) => m.ContactComponent),
+  },
+];
